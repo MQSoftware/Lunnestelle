c9033befbef798807f37e159a28bd2a04d6e7815
myac-2022020@kinal.edu.gt <myac-2022020@kinal.edu.gt>
2025-08-06 02:17:07 -0600

    Lunnestelle version 1.0.1.3

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ window.addEventListener('scroll', () => {
   }
 })
 
+const slider = document.querySelector('.slider ul')
+const totalImages = document.querySelectorAll('.slider li').length
+
+let index = 0
+
+setInterval(() => {
+  index = (index + 1) % totalImages // Avanza y vuelve a 0 al final
+  slider.style.transform = `translateX(-${index * 100}%)`
+}, 3000) // Cambia cada 3 segundos
 
 
 const about = document.querySelector('.aboutUsBox');
